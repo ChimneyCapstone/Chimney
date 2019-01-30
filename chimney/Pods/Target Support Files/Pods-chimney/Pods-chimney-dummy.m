6079a4721c5d6122fc7796ba4a9b2067c6b76243
Kangwoo Choi <kangwooc@gmail.com>
2019-01-29 17:38:21 -0800

    initial commit

---
+++ chimney/Pods/Target Support Files/Pods-chimney/Pods-chimney-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_chimney : NSObject
+@end
+@implementation PodsDummy_Pods_chimney
+@end
---
+++ chimney/Pods/Target Support Files/Pods-chimneyUITests/Pods-chimneyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_chimneyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_chimneyUITests
+@end

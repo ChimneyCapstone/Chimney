207a2399f79ae5f1c8a6048afcbc11280c722c1e
Kangwoo Choi <kangwooc@gmail.com>
2019-03-08 23:24:19 -0800

    add auto-complete function on phoneTextField

--- chimney/chimney/SignUpViewController.swift
+++ chimney/chimney/SignUpViewController.swift
@@ -5,15 +5,17 @@
 //  Created by Kangwoo on 3/2/19.
 //  Copyright © 2019 chimney. All rights reserved.
 //
-// need to add phone number
-
+//
 
 import Foundation
 import UIKit
 import FirebaseAuth // for authenication
 import Firebase
 
-class SignUpViewController: UIViewController{
+// Reference: https://stackoverflow.com/questions/1246439/uitextfield-for-phone-number
+class SignUpViewController: UIViewController, UITextFieldDelegate {
+    
+    var myPhoneNumber = String()
     // text fields
     @IBOutlet weak var fullNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
@@ -28,10 +30,28 @@ class SignUpViewController: UIViewController{
         super.viewDidLoad()
         // initialize the reference
         ref = Database.database().reference()
+        phoneTextField.delegate = self
+        phoneTextField.keyboardType = .phonePad
     }
    
-    @IBAction func phoneTextEdit(_ sender: Any) {
-        
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if (textField == self.phoneTextField) && textField.text == ""{
+            textField.text = "+1 (" //your country code default
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == phoneTextField {
+            let res = phoneMask(phoneTextField: phoneTextField, textField: textField, range, string)
+            myPhoneNumber = res.phoneNumber != "" ? "+\(res.phoneNumber)" : ""
+            print("Phone - \(myPhoneNumber)  MaskPhone=\(res.maskPhoneNumber)")
+            if (res.phoneNumber.count == 11) || (res.phoneNumber.count == 0) {
+                //phone number entered or completely cleared
+                print("EDIT END: Phone = \(myPhoneNumber)  MaskPhone = \(res.maskPhoneNumber)")
+            }
+            return res.result
+        }
+        return true
     }
     
     // after tapping sign up button
@@ -89,9 +109,9 @@ class SignUpViewController: UIViewController{
     }
     
     // check if it's valid phone number
-    // https://stackoverflow.com/questions/53427424/how-to-validate-american-phone-format-in-swift-4
+    // Reference: https://stackoverflow.com/questions/53427424/how-to-validate-american-phone-format-in-swift-4
     func isValidMobile(testStr:String) -> Bool {
-        let mobileRegEx = "^[1{1}]\\s\\d{3}-\\d{3}-\\d{4}$"
+        let mobileRegEx = "(\\([0-9]{3}\\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}"
         let mobileTest = NSPredicate(format:"SELF MATCHES %@", mobileRegEx)
         return mobileTest.evaluate(with: testStr)
     }
@@ -105,4 +125,93 @@ class SignUpViewController: UIViewController{
         }
     }
     
+}
+
+extension UITextFieldDelegate {
+    func phoneMask(phoneTextField: UITextField, textField: UITextField, _ range: NSRange, _ string: String) -> (result: Bool, phoneNumber: String, maskPhoneNumber: String) {
+        let oldString = textField.text!
+        let newString = oldString.replacingCharacters(in: Range(range, in: oldString)!, with: string)
+        //in numString only Numeric characters
+        let components = newString.components(separatedBy: CharacterSet.decimalDigits.inverted)
+        let numString = components.joined(separator: "")
+        
+        let length = numString.count
+        let maxCharInPhone = 11
+        
+        if newString.count < oldString.count { //backspace to work
+            if newString.count <= 2 { //if now "+7(" and push backspace
+                phoneTextField.text = ""
+                return (false, "", "")
+            } else {
+                return (true, numString, newString) //will not in the process backspace
+            }
+        }
+        
+        if length > maxCharInPhone { // input is complete, do not add characters
+            return (false, numString, newString)
+        }
+        var indexStart, indexEnd: String.Index
+        var maskString = "", template = ""
+        var endOffset = 0
+        
+        if newString == "+" { // allow add "+" if first Char
+            maskString += "+"
+        }
+        //format +X(XXX)XXX-XXXX
+        if length > 0 {
+            maskString += "+"
+            indexStart = numString.index(numString.startIndex, offsetBy: 0)
+            indexEnd = numString.index(numString.startIndex, offsetBy: 1)
+            maskString += String(numString[indexStart..<indexEnd]) + "("
+        }
+        if length > 1 {
+            endOffset = 4
+            template = ")"
+            if length < 4 {
+                endOffset = length
+                template = ""
+            }
+            indexStart = numString.index(numString.startIndex, offsetBy: 1)
+            indexEnd = numString.index(numString.startIndex, offsetBy: endOffset)
+            maskString += String(numString[indexStart..<indexEnd]) + template
+        }
+        if length > 4 {
+            endOffset = 7
+            template = "-"
+            if length < 7 {
+                endOffset = length
+                template = ""
+            }
+            indexStart = numString.index(numString.startIndex, offsetBy: 4)
+            indexEnd = numString.index(numString.startIndex, offsetBy: endOffset)
+            maskString += String(numString[indexStart..<indexEnd]) + template
+        }
+        var nIndex: Int; nIndex = 7
+        //            //format +X(XXX)XXX-XX-XX  -> if need uncoment
+        //            nIndex = 9
+        //
+        //            if length > 7 {
+        //                endOffset = 9
+        //                template = "-"
+        //                if length < 9 {
+        //                    endOffset = length
+        //                    template = ""
+        //                }
+        //                indexStart = numString.index(numString.startIndex, offsetBy: 7)
+        //                indexEnd = numString.index(numString.startIndex, offsetBy: endOffset)
+        //                maskString += String(numString[indexStart..<indexEnd]) + template
+        //            }
+        if length > nIndex {
+            indexStart = numString.index(numString.startIndex, offsetBy: nIndex)
+            indexEnd = numString.index(numString.startIndex, offsetBy: length)
+            maskString += String(numString[indexStart..<indexEnd])
+        }
+        phoneTextField.text = maskString
+        if length == maxCharInPhone {
+            //dimiss kayboard
+            phoneTextField.endEditing(true)
+            return (false, numString, newString)
+        }
+        return (false, numString, newString)
+    }
 }

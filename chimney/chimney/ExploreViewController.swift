c7629a77e82f61fbf3c4a1ab7d37a20eebfb8975
uwd73d8g <tina.zhuang.t.z@gmail.com>
2019-04-17 16:15:26 -0700

    rendered table

--- chimney/chimney/ExploreViewController.swift
+++ chimney/chimney/ExploreViewController.swift
@@ -10,46 +10,6 @@ import Foundation
 import UIKit
 import Firebase
 
-
-//class ExploreViewController: UIViewController, UITableViewDataSource {
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 7
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = UITableViewCell()
-//
-//        var ref: DatabaseReference!
-////        let uid = Auth.auth().currentUser!.uid
-//        ref = Database.database().reference().child("users");
-//        ref.observeSingleEvent(of: .value) { snapshot in
-//            print(snapshot)
-//            print(snapshot.childrenCount) // I got the expected number of items
-//            let enumerator = snapshot.children
-//            while let rest = enumerator.nextObject() as? DataSnapshot {
-//                print(rest.value)
-//                cell.textLabel?.text = rest.value as! String;
-//            }
-//        }
-//
-////        cell.addSubview(label)
-//        return cell
-//
-//    }
-//
-//
-//    @IBOutlet weak var ExploreTableView: UITableView!
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-////        navigationItem.title = quake.place
-//
-////        ExploreTableView.datasource = self
-//    }
-//
-//
-//}
-
 class ExploreViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var contents:[String] = []
@@ -108,20 +68,51 @@ class ExploreViewController: UIViewController, UITableViewDataSource, UITableVie
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Create an object of the dynamic cell "PlainCell"
-        self.index+=1
         let cell = tableView.dequeueReusableCell(withIdentifier: "PlainCell", for: indexPath)
+        // Create an object of the dynamic cell "PlainCell"
+        var ref: DatabaseReference!
+        ref = Database.database().reference().child("users");
+        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            for child in snapshot.children {
+                let snap = child as! DataSnapshot
+                let key = snap.key
+                if let snapshots = snapshot.children.allObjects as? [DataSnapshot] {
+                    for snap in snapshots {
+                        if (snap.value as? Dictionary<String, AnyObject>) != nil {
+                            let key = snap.key
+                            let value = snap.value as? Dictionary<String, AnyObject>
+                            if value?["request"] != nil {
+                                let content = value?["request"] as? Dictionary<String, Dictionary<String, String>>
+                                let val = content!.values
+                                for (a) in val{
+                                    let task: String = "amount $: \(a["amount"]!)    task:  \(a["task"]!)";
+                                    self.contents.append(task)
+                                    //                                    print(self.contents)
+//                                    cell.textLabel?.text = self.contents[self.index]
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+        });
         // Depending on the section, fill the textLabel with the relevant text
-        
-        loadInfo()
-        print("second")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) {
+            if self.index < self.contents.count {
+                self.index+=1
+                cell.textLabel?.text = self.contents[self.index]
+//                print(self.contents[self.index])
+
+            }
+        }
+//        loadInfo()
+//        print("second")
 //        print(self.index)
 //        print(whole)
-        if self.index < self.contents.count {
-            cell.textLabel?.text = self.contents[self.index]
-            print(self.contents[self.index])
-        }
+        print(cell.textLabel?.text)
         return cell
+
 
     }
 }

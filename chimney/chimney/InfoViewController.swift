0cb87b44df689a3dad84295c78eb72936dd63a91
uwd73d8g <tina.zhuang.t.z@gmail.com>
2019-05-22 21:45:09 -0700

    connected msg and map

--- chimney/chimney/InfoViewController.swift
+++ chimney/chimney/InfoViewController.swift
@@ -8,17 +8,33 @@
 
 import Foundation
 import UIKit
-
+import CoreLocation
+import MapKit
 class InfoViewController: UIViewController{
     
     @IBAction func MapButtonTapped(_ sender: UIButton) {
-        if (UIApplication.shared.canOpenURL(NSURL(string:"comgooglemaps://")! as URL)) {
-            UIApplication.shared.openURL(NSURL(string:
-                "comgooglemaps://?saddr=&daddr=47.6593549,-122.30366374805264&directionsmode=driving")! as URL)
-
-        } else {
-            NSLog("Can't use comgooglemaps://");
-        }
+        let latitude: CLLocationDegrees = 47.6062
+        let longitude: CLLocationDegrees = 122.3321
+        
+        let regionDistance:CLLocationDistance = 10000
+        let coordinates = CLLocationCoordinate2DMake(latitude, longitude)
+        let regionSpan = MKCoordinateRegion(center: coordinates, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
+        let options = [
+            MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center),
+            MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)
+        ]
+        let placemark = MKPlacemark(coordinate: coordinates, addressDictionary: nil)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = "Dave's place"
+        mapItem.openInMaps(launchOptions: options)
+    }
+    
+    @IBAction func MessageButtonTapped(_ sender: UIButton) {
+//        if UIApplication.shared.canOpenURL(URL(string:"sms:2062281234")!) {
+//            UIApplication.shared.open(URL(string:"sms:2062281234")!, options: [:], completionHandler: nil)
+//        }
+        let sms: String = "sms:2061231231&body=Hello, I am on my way."
+        let strURL: String = sms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        UIApplication.shared.open(URL.init(string: strURL)!, options: [:], completionHandler: nil)
     }
 }
-
